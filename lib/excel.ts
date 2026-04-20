16e825cdfcbd14366ce0fbf76835cf5782ae34eb
jjpiersoniv <j.j.pierson.iv@gmail.com>
2026-04-20 16:10:50 -0400

    Update excel.ts

--- lib/excel.ts
+++ lib/excel.ts
@@ -1,7 +1,7 @@
 import * as XLSX from 'xlsx'
 import { ScrapeResult, computeStats } from './scraper'
 
-export function buildExcel(results: ScrapeResult[]): Buffer {
+export function buildExcel(results: ScrapeResult[]): Uint8Array {
   const wb = XLSX.utils.book_new()
 
   for (const result of results) {
